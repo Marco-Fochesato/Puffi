7897b03ec130b9b7fe4d3ed83499232ea6d536e3
Marco-Fochesato <14529834@itisrossi.vi.it>
2026-03-02 19:27:22 +0100

    Versione con punti 0, 1, 2

---
+++ Puffi/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Puffi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlArea = new Panel();
+            pnlCasa = new Panel();
+            pnlPuffo = new Panel();
+            txtPunti = new TextBox();
+            pnlArea.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlArea
+            // 
+            pnlArea.BackColor = Color.FromArgb(192, 255, 192);
+            pnlArea.BorderStyle = BorderStyle.FixedSingle;
+            pnlArea.Controls.Add(pnlCasa);
+            pnlArea.Controls.Add(pnlPuffo);
+            pnlArea.Location = new Point(0, 0);
+            pnlArea.Name = "pnlArea";
+            pnlArea.Size = new Size(1938, 1172);
+            pnlArea.TabIndex = 0;
+            // 
+            // pnlCasa
+            // 
+            pnlCasa.BackColor = Color.Red;
+            pnlCasa.Location = new Point(1132, 431);
+            pnlCasa.Name = "pnlCasa";
+            pnlCasa.Size = new Size(200, 200);
+            pnlCasa.TabIndex = 1;
+            // 
+            // pnlPuffo
+            // 
+            pnlPuffo.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+            pnlPuffo.BackColor = Color.LightBlue;
+            pnlPuffo.Location = new Point(0, 0);
+            pnlPuffo.Name = "pnlPuffo";
+            pnlPuffo.Size = new Size(50, 100);
+            pnlPuffo.TabIndex = 0;
+            // 
+            // txtPunti
+            // 
+            txtPunti.AcceptsTab = true;
+            txtPunti.BackColor = Color.FromArgb(224, 224, 224);
+            txtPunti.Location = new Point(1953, 28);
+            txtPunti.Name = "txtPunti";
+            txtPunti.ReadOnly = true;
+            txtPunti.Size = new Size(194, 39);
+            txtPunti.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2156, 1216);
+            Controls.Add(txtPunti);
+            Controls.Add(pnlArea);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += PressTasto;
+            pnlArea.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlArea;
+        private Panel pnlPuffo;
+        private Panel pnlCasa;
+        private TextBox txtPunti;
+    }
+}
---
+++ Puffi/Form1.cs
@@ -0,0 +1,136 @@
+namespace Puffi
+{
+    struct Punto
+    {
+        public int x;
+        public int y;
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        Punto pos = new Punto();
+        int punti = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            pos.x = 0;
+            pos.y = 0;
+            pnlPuffo.Location = new Point(pos.x, pos.y);
+
+            // Mette la casa in una posizione casuale all'interno dell'area
+            var rand = new Random();
+            int maxX = Math.Max(0, pnlArea.ClientSize.Width - pnlCasa.Width);
+            int maxY = Math.Max(0, pnlArea.ClientSize.Height - pnlCasa.Height);
+            int x = rand.Next(0, maxX + 1);
+            int y = rand.Next(0, maxY + 1);
+            pnlCasa.Location = new Point(x, y);
+        }
+
+        private void PressTasto(object sender, KeyEventArgs e)
+        {
+            int passo= 20; // Distanza in pixel per ogni spostamento (clcik di tasto)
+
+            switch (e.KeyCode)
+            {
+                case Keys.S:
+                    {
+                        pos.y += passo;
+                        if (pos.y > pnlArea.Height - pnlPuffo.Height) // se con il prossimo passo rischia di uscire dall'area 
+                        {
+                            pos.y = pnlArea.Height - pnlPuffo.Height; // limita la posizione al bordo inferiore
+                        }
+                        pnlPuffo.Location = new Point(pos.x, pos.y);
+
+                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        {
+                            punti++; // Incrementa i punti
+                            txtPunti.Text = "Punti: " + punti.ToString(); //mette i tunti sulla txt
+                            var rand = new Random();
+                            int maxX = Math.Max(0, pnlArea.ClientSize.Width - pnlCasa.Width); // trova max in x
+                            int maxY = Math.Max(0, pnlArea.ClientSize.Height - pnlCasa.Height); // trova max in y
+                            int x = rand.Next(0, maxX + 1); // genera un numero casuale in x rispettando max
+                            int y = rand.Next(0, maxY + 1); // genera un numero casuale in y rispettando max
+                            pnlCasa.Location = new Point(x, y); //sposta la casa
+                        }
+                    }
+                    break;
+                case Keys.W:
+                    {
+                        pos.y -= passo;
+                        if (pos.y < 0)
+                        {
+                            pos.y = 0; // Limita la posizione al bordo superiore
+                        }
+                        pnlPuffo.Location = new Point(pos.x, pos.y);
+                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        {
+                            punti++;
+                            txtPunti.Text = "Punti: " + punti.ToString();
+                            var rand = new Random();
+                            int maxX = Math.Max(0, pnlArea.ClientSize.Width - pnlCasa.Width);
+                            int maxY = Math.Max(0, pnlArea.ClientSize.Height - pnlCasa.Height);
+                            int x = rand.Next(0, maxX + 1);
+                            int y = rand.Next(0, maxY + 1);
+                            pnlCasa.Location = new Point(x, y);
+                        }
+                    }
+                    break;
+                case Keys.A:
+                    {
+                        pos.x -= passo;
+                        if (pos.x < 0)
+                        {
+                            pos.x = 0; // Limita la posizione al bordo sinistro
+                        }
+                        pnlPuffo.Location = new Point(pos.x, pos.y);
+                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        {
+                            punti++;
+                            txtPunti.Text = "Punti: " + punti.ToString();
+                            var rand = new Random();
+                            int maxX = Math.Max(0, pnlArea.ClientSize.Width - pnlCasa.Width);
+                            int maxY = Math.Max(0, pnlArea.ClientSize.Height - pnlCasa.Height);
+                            int x = rand.Next(0, maxX + 1);
+                            int y = rand.Next(0, maxY + 1);
+                            pnlCasa.Location = new Point(x, y);
+                        }
+                    }
+                    break;
+                case Keys.D:
+                    {
+                        pos.x += passo;
+                        if (pos.x > pnlArea.Width - pnlPuffo.Width)
+                        {
+                            pos.x = pnlArea.Width - pnlPuffo.Width; // Limita la posizione al bordo destro
+                        }
+                        pnlPuffo.Location = new Point(pos.x, pos.y);
+                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        {
+                            punti++;
+                            txtPunti.Text = "Punti: " + punti.ToString();
+                            var rand = new Random();
+                            int maxX = Math.Max(0, pnlArea.ClientSize.Width - pnlCasa.Width);
+                            int maxY = Math.Max(0, pnlArea.ClientSize.Height - pnlCasa.Height);
+                            int x = rand.Next(0, maxX + 1);
+                            int y = rand.Next(0, maxY + 1);
+                            pnlCasa.Location = new Point(x, y);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
