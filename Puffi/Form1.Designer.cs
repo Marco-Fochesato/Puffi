11426bcad486998028297b58a66fcb9d50c8c37e
Marco-Fochesato <14529834@itisrossi.vi.it>
2026-03-03 23:21:12 +0100

    aggiunta punti 3 e 4

--- Puffi/Form1.Designer.cs
+++ Puffi/Form1.Designer.cs
@@ -29,22 +29,135 @@
         private void InitializeComponent()
         {
             pnlArea = new Panel();
+            pnl10 = new Panel();
+            pnl9 = new Panel();
+            pnl8 = new Panel();
+            pnl7 = new Panel();
+            pnl6 = new Panel();
+            pnl5 = new Panel();
+            pnl4 = new Panel();
+            pnl3 = new Panel();
+            pnl2 = new Panel();
+            pnl1 = new Panel();
+            pnlGarga = new Panel();
             pnlCasa = new Panel();
             pnlPuffo = new Panel();
-            txtPunti = new TextBox();
+            txtPuntiPuffo = new TextBox();
+            lblPuntiP = new Label();
+            lblPubtiG = new Label();
+            txtPuntiGarga = new TextBox();
             pnlArea.SuspendLayout();
             SuspendLayout();
             // 
             // pnlArea
             // 
-            pnlArea.BackColor = Color.FromArgb(192, 255, 192);
+            pnlArea.BackColor = Color.Beige;
             pnlArea.BorderStyle = BorderStyle.FixedSingle;
+            pnlArea.Controls.Add(pnl10);
+            pnlArea.Controls.Add(pnl9);
+            pnlArea.Controls.Add(pnl8);
+            pnlArea.Controls.Add(pnl7);
+            pnlArea.Controls.Add(pnl6);
+            pnlArea.Controls.Add(pnl5);
+            pnlArea.Controls.Add(pnl4);
+            pnlArea.Controls.Add(pnl3);
+            pnlArea.Controls.Add(pnl2);
+            pnlArea.Controls.Add(pnl1);
+            pnlArea.Controls.Add(pnlGarga);
             pnlArea.Controls.Add(pnlCasa);
             pnlArea.Controls.Add(pnlPuffo);
             pnlArea.Location = new Point(0, 0);
             pnlArea.Name = "pnlArea";
             pnlArea.Size = new Size(1938, 1172);
             pnlArea.TabIndex = 0;
+            // 
+            // pnl10
+            // 
+            pnl10.BackColor = Color.Green;
+            pnl10.Location = new Point(1588, 607);
+            pnl10.Name = "pnl10";
+            pnl10.Size = new Size(50, 120);
+            pnl10.TabIndex = 12;
+            // 
+            // pnl9
+            // 
+            pnl9.BackColor = Color.Green;
+            pnl9.Location = new Point(1018, 855);
+            pnl9.Name = "pnl9";
+            pnl9.Size = new Size(50, 120);
+            pnl9.TabIndex = 11;
+            // 
+            // pnl8
+            // 
+            pnl8.BackColor = Color.Green;
+            pnl8.Location = new Point(400, 146);
+            pnl8.Name = "pnl8";
+            pnl8.Size = new Size(50, 120);
+            pnl8.TabIndex = 10;
+            // 
+            // pnl7
+            // 
+            pnl7.BackColor = Color.Green;
+            pnl7.Location = new Point(428, 793);
+            pnl7.Name = "pnl7";
+            pnl7.Size = new Size(50, 120);
+            pnl7.TabIndex = 9;
+            // 
+            // pnl6
+            // 
+            pnl6.BackColor = Color.Green;
+            pnl6.Location = new Point(1454, 259);
+            pnl6.Name = "pnl6";
+            pnl6.Size = new Size(50, 120);
+            pnl6.TabIndex = 8;
+            // 
+            // pnl5
+            // 
+            pnl5.BackColor = Color.Green;
+            pnl5.Location = new Point(1292, 751);
+            pnl5.Name = "pnl5";
+            pnl5.Size = new Size(50, 120);
+            pnl5.TabIndex = 7;
+            // 
+            // pnl4
+            // 
+            pnl4.BackColor = Color.Green;
+            pnl4.Location = new Point(848, 259);
+            pnl4.Name = "pnl4";
+            pnl4.Size = new Size(50, 120);
+            pnl4.TabIndex = 6;
+            // 
+            // pnl3
+            // 
+            pnl3.BackColor = Color.Green;
+            pnl3.Location = new Point(828, 633);
+            pnl3.Name = "pnl3";
+            pnl3.Size = new Size(50, 120);
+            pnl3.TabIndex = 5;
+            // 
+            // pnl2
+            // 
+            pnl2.BackColor = Color.Green;
+            pnl2.Location = new Point(943, 525);
+            pnl2.Name = "pnl2";
+            pnl2.Size = new Size(50, 120);
+            pnl2.TabIndex = 4;
+            // 
+            // pnl1
+            // 
+            pnl1.BackColor = Color.Green;
+            pnl1.Location = new Point(1108, 135);
+            pnl1.Name = "pnl1";
+            pnl1.Size = new Size(50, 120);
+            pnl1.TabIndex = 3;
+            // 
+            // pnlGarga
+            // 
+            pnlGarga.BackColor = Color.Black;
+            pnlGarga.Location = new Point(550, 345);
+            pnlGarga.Name = "pnlGarga";
+            pnlGarga.Size = new Size(50, 100);
+            pnlGarga.TabIndex = 2;
             // 
             // pnlCasa
             // 
@@ -56,29 +169,57 @@
             // 
             // pnlPuffo
             // 
-            pnlPuffo.Anchor = AnchorStyles.Top | AnchorStyles.Left;
             pnlPuffo.BackColor = Color.LightBlue;
             pnlPuffo.Location = new Point(0, 0);
             pnlPuffo.Name = "pnlPuffo";
             pnlPuffo.Size = new Size(50, 100);
             pnlPuffo.TabIndex = 0;
             // 
-            // txtPunti
+            // txtPuntiPuffo
             // 
-            txtPunti.AcceptsTab = true;
-            txtPunti.BackColor = Color.FromArgb(224, 224, 224);
-            txtPunti.Location = new Point(1953, 28);
-            txtPunti.Name = "txtPunti";
-            txtPunti.ReadOnly = true;
-            txtPunti.Size = new Size(194, 39);
-            txtPunti.TabIndex = 1;
+            txtPuntiPuffo.AcceptsTab = true;
+            txtPuntiPuffo.BackColor = Color.FromArgb(224, 224, 224);
+            txtPuntiPuffo.Location = new Point(1950, 98);
+            txtPuntiPuffo.Name = "txtPuntiPuffo";
+            txtPuntiPuffo.ReadOnly = true;
+            txtPuntiPuffo.Size = new Size(194, 39);
+            txtPuntiPuffo.TabIndex = 1;
+            // 
+            // lblPuntiP
+            // 
+            lblPuntiP.AutoSize = true;
+            lblPuntiP.Location = new Point(1961, 38);
+            lblPuntiP.Name = "lblPuntiP";
+            lblPuntiP.Size = new Size(160, 32);
+            lblPuntiP.TabIndex = 2;
+            lblPuntiP.Text = "Punti di Puffo";
+            // 
+            // lblPubtiG
+            // 
+            lblPubtiG.AutoSize = true;
+            lblPubtiG.Location = new Point(1935, 170);
+            lblPubtiG.Name = "lblPubtiG";
+            lblPubtiG.Size = new Size(223, 32);
+            lblPubtiG.TabIndex = 3;
+            lblPubtiG.Text = "Punti di Gargamella";
+            // 
+            // txtPuntiGarga
+            // 
+            txtPuntiGarga.BackColor = Color.FromArgb(224, 224, 224);
+            txtPuntiGarga.Location = new Point(1961, 228);
+            txtPuntiGarga.Name = "txtPuntiGarga";
+            txtPuntiGarga.Size = new Size(170, 39);
+            txtPuntiGarga.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2156, 1216);
-            Controls.Add(txtPunti);
+            Controls.Add(txtPuntiGarga);
+            Controls.Add(lblPubtiG);
+            Controls.Add(lblPuntiP);
+            Controls.Add(txtPuntiPuffo);
             Controls.Add(pnlArea);
             KeyPreview = true;
             Name = "Form1";
@@ -95,6 +236,20 @@
         private Panel pnlArea;
         private Panel pnlPuffo;
         private Panel pnlCasa;
-        private TextBox txtPunti;
+        private TextBox txtPuntiPuffo;
+        private Panel pnlGarga;
+        private Label lblPuntiP;
+        private Label lblPubtiG;
+        private TextBox txtPuntiGarga;
+        private Panel pnl3;
+        private Panel pnl2;
+        private Panel pnl1;
+        private Panel pnl5;
+        private Panel pnl4;
+        private Panel pnl10;
+        private Panel pnl9;
+        private Panel pnl8;
+        private Panel pnl7;
+        private Panel pnl6;
     }
 }
--- Puffi/Form1.cs
+++ Puffi/Form1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Puffi
 {
     struct Punto
@@ -14,103 +16,346 @@ namespace Puffi
 
     public partial class Form1 : Form
     {
-        Punto pos = new Punto();
-        int punti = 0;
+        Punto posCasa = new Punto();
+        Punto posPuffo = new Punto();
+        Punto posGarga = new Punto();
+        Punto[] posAlberi = new Punto[10];
+        Panel[] pnlAlberi = new Panel[10];
+        int puntiPuffo = 0;
+        int puntiGarga = 0;
+        bool turnoPuffo = true;
+        int contatorePassi = 0;
         public Form1()
         {
             InitializeComponent();
+            // Inizializza l'array dei pannelli degli alberi
+            pnlAlberi[0] = pnl1;
+            pnlAlberi[1] = pnl2;
+            pnlAlberi[2] = pnl3;
+            pnlAlberi[3] = pnl4;
+            pnlAlberi[4] = pnl5;
+            pnlAlberi[5] = pnl6;
+            pnlAlberi[6] = pnl7;
+            pnlAlberi[7] = pnl8;
+            pnlAlberi[8] = pnl9;
+            pnlAlberi[9] = pnl10;
         }
 
-        
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            pos.x = 0;
-            pos.y = 0;
-            pnlPuffo.Location = new Point(pos.x, pos.y);
+            posCasa.x = 0;
+            posCasa.y = 0;
+            pnlCasa.Location = new Point(posCasa.x, posCasa.y);
             SpostaCasa(false); // Posiziona la casa in una posizione casuale all'avvio senza incrementare i punti
+            SpostaAlberi(); // Posiziona gli alberi randomicamente all'avvio
         }
 
         private void PressTasto(object sender, KeyEventArgs e)
         {
-            int passo= 20; // Distanza in pixel per ogni spostamento (clcik di tasto)
+            int passo = 20; // Distanza in pixel per ogni spostamento (clcik di tasto)
 
             switch (e.KeyCode)
             {
                 case Keys.S:
                     {
-                        pos.y += passo;
-                        if (pos.y > pnlArea.Height - pnlPuffo.Height) // se con il prossimo passo rischia di uscire dall'area 
-                        {
-                            pos.y = pnlArea.Height - pnlPuffo.Height; // limita la posizione al bordo inferiore
-                        }
-                        pnlPuffo.Location = new Point(pos.x, pos.y);
+                        if (!turnoPuffo) return; // Se non è il turno di Puffo, ignora
 
-                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        posPuffo.y += passo;
+                        if (posPuffo.y > pnlArea.Height - pnlPuffo.Height) // se con il prossimo passo rischia di uscire dall'area 
                         {
-                            SpostaCasa(true);
+                            posPuffo.y = pnlArea.Height - pnlPuffo.Height; // limita la posizione al bordo inferiore
+                        }
+                        pnlPuffo.Location = new Point(posPuffo.x, posPuffo.y);
+
+                        contatorePassi++;
+                        if (!CollidePulse()) // Se non collide con albero, continua normalmente
+                        {
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = false; // Perde il turno
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
                         }
                     }
                     break;
                 case Keys.W:
                     {
-                        pos.y -= passo;
-                        if (pos.y < 0)
+                        if (!turnoPuffo) return; // Se non è il turno di Puffo, ignora
+
+                        posPuffo.y -= passo;
+                        if (posPuffo.y < 0)
                         {
-                            pos.y = 0; // Limita la posizione al bordo superiore
+                            posPuffo.y = 0; // Limita la posizione al bordo superiore
                         }
-                        pnlPuffo.Location = new Point(pos.x, pos.y);
-                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        pnlPuffo.Location = new Point(posPuffo.x, posPuffo.y);
+
+                        contatorePassi++;
+                        if (!CollidePulse()) // Se non collide con albero, continua normalmente
                         {
-                            SpostaCasa(true);
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = false; // Perde il turno
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
                         }
                     }
                     break;
                 case Keys.A:
                     {
-                        pos.x -= passo;
-                        if (pos.x < 0)
+                        if (!turnoPuffo) return; // Se non è il turno di Puffo, ignora
+
+                        posPuffo.x -= passo;
+                        if (posPuffo.x < 0)
                         {
-                            pos.x = 0; // Limita la posizione al bordo sinistro
+                            posPuffo.x = 0; // Limita la posizione al bordo sinistro
                         }
-                        pnlPuffo.Location = new Point(pos.x, pos.y);
-                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        pnlPuffo.Location = new Point(posPuffo.x, posPuffo.y);
+
+                        contatorePassi++;
+                        if (!CollidePulse()) // Se non collide con albero, continua normalmente
                         {
-                            SpostaCasa(true);
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = false; // Perde il turno
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
                         }
                     }
                     break;
                 case Keys.D:
                     {
-                        pos.x += passo;
-                        if (pos.x > pnlArea.Width - pnlPuffo.Width)
+                        if (!turnoPuffo) return; // Se non è il turno di Puffo, ignora
+
+                        posPuffo.x += passo;
+                        if (posPuffo.x > pnlArea.Width - pnlPuffo.Width)
                         {
-                            pos.x = pnlArea.Width - pnlPuffo.Width; // Limita la posizione al bordo destro
+                            posPuffo.x = pnlArea.Width - pnlPuffo.Width; // Limita la posizione al bordo destro
                         }
-                        pnlPuffo.Location = new Point(pos.x, pos.y);
-                        if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+                        pnlPuffo.Location = new Point(posPuffo.x, posPuffo.y);
+
+                        contatorePassi++;
+                        if (!CollidePulse()) // Se non collide con albero, continua normalmente
                         {
-                            SpostaCasa(true);
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = false; // Perde il turno
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
+                        }
+                    }
+                    break;
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.I:
+                    {
+                        if (turnoPuffo) return; // Se è il turno di Puffo, ignora
+
+                        posGarga.y -= passo;
+                        if (posGarga.y < 0)
+                        {
+                            posGarga.y = 0; // Limita la posizione al bordo superiore
+                        }
+                        pnlGarga.Location = new Point(posGarga.x, posGarga.y);
+
+                        contatorePassi++;
+                        if (!CollideGarga()) // Se non collide con albero, continua normalmente
+                        {
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = true; // Perde il turno, passa a Puffo
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
+                        }
+                    }
+                    break;
+                case Keys.K:
+                    {
+                        if (turnoPuffo) return; // Se è il turno di Puffo, ignora
+
+                        posGarga.y += passo;
+                        if (posGarga.y > pnlArea.Height - pnlGarga.Height)
+                        {
+                            posGarga.y = pnlArea.Height - pnlGarga.Height; // Limita la posizione al bordo inferiore
+                        }
+                        pnlGarga.Location = new Point(posGarga.x, posGarga.y);
+
+                        contatorePassi++;
+                        if (!CollideGarga()) // Se non collide con albero, continua normalmente
+                        {
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = true; // Perde il turno, passa a Puffo
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
+                        }
+                    }
+                    break;
+                case Keys.L:
+                    {
+                        if (turnoPuffo) return; // Se è il turno di Puffo, ignora
+
+                        posGarga.x += passo;
+                        if (posGarga.x > pnlArea.Width - pnlGarga.Width)
+                        {
+                            posGarga.x = pnlArea.Width - pnlGarga.Width; // Limita la posizione al bordo destro
+                        }
+                        pnlGarga.Location = new Point(posGarga.x, posGarga.y);
+
+                        contatorePassi++;
+                        if (!CollideGarga()) // Se non collide con albero, continua normalmente
+                        {
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = true; // Perde il turno, passa a Puffo
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
+                        }
+                    }
+                    break;
+                case Keys.J:
+                    {
+                        if (turnoPuffo) return; // Se è il turno di Puffo, ignora
+
+                        posGarga.x -= passo;
+                        if (posGarga.x < 0)
+                        {
+                            posGarga.x = 0; // Limita la posizione al bordo sinistro
+                        }
+                        pnlGarga.Location = new Point(posGarga.x, posGarga.y);
+
+                        contatorePassi++;
+                        if (!CollideGarga()) // Se non collide con albero, continua normalmente
+                        {
+                            CambiaTurno();
+                            Collisione();
+                        }
+                        else
+                        {
+                            contatorePassi--; // Annulla l'incremento del contatore se c'è collisione
+                            turnoPuffo = true; // Perde il turno, passa a Puffo
+                            CambiaTurno(); // Passa direttamente al turno dell'avversario
                         }
                     }
                     break;
             }
         }
-        private void SpostaCasa(bool incrPunti )
+        private void CambiaTurno()
+        {
+            if (contatorePassi >= 10)
+            {
+                contatorePassi = 0;
+                turnoPuffo = !turnoPuffo;
+            }
+        }
+        private void Collisione()
+        {
+            if (pnlPuffo.Bounds.IntersectsWith(pnlCasa.Bounds))
+            {
+                SpostaCasa(true);
+            }
+            if (pnlGarga.Bounds.IntersectsWith(pnlPuffo.Bounds))
+            {
+                SpostaPuffo();
+            }
+        }
+        private void SpostaCasa(bool incrPunti)
         {
             if (incrPunti)
             {
-                punti++; // Incrementa i punti
-                txtPunti.Text = "Punti: " + punti.ToString(); //mette i punti sulla txtPunti
+                puntiPuffo++; // Incrementa i punti
+                txtPuntiPuffo.Text = "Punti: " + puntiPuffo.ToString(); //mette i punti sulla txtPunti
+                SpostaAlberi(); // Sposta gli alberi quando Puffo raggiunge la casa
             }
-            var rand = new Random();
-            int maxX = Math.Max(0, pnlArea.Width - pnlCasa.Width); // trova max in x
-            int maxY = Math.Max(0, pnlArea.Height - pnlCasa.Height); // trova max in y
+            Random rand = new Random();
+            int maxX = pnlArea.Width - pnlCasa.Width;  // trova max in x
+            int maxY = pnlArea.Height - pnlCasa.Height;  // trova max in y
             int x = rand.Next(0, maxX + 1); // genera un numero casuale in x rispettando max
             int y = rand.Next(0, maxY + 1); // genera un numero casuale in y rispettando max
             pnlCasa.Location = new Point(x, y); //sposta la casa
-            
+
+        }
+
+        private void SpostaPuffo()
+        {
+            puntiGarga++; // Incrementa i punti
+            puntiPuffo--; // Decrementa i punti di Puffo
+            txtPuntiGarga.Text = "Punti: " + puntiGarga.ToString(); //mette i punti sulla txtPunti
+            txtPuntiPuffo.Text = "Punti: " + puntiPuffo.ToString(); //mette i punti sulla txtPunti
+            Random rand = new Random();
+            int maxX = pnlArea.Width - pnlPuffo.Width;  // trova max in x
+            int maxY = pnlArea.Height - pnlPuffo.Height;  // trova max in y
+            int x = rand.Next(0, maxX + 1); // genera un numero casuale in x rispettando max
+            int y = rand.Next(0, maxY + 1); // genera un numero casuale in y rispettando max
+            posPuffo.x = x;  // AGGIORNA posPuffo
+            posPuffo.y = y;  // AGGIORNA posPuffo
+            pnlPuffo.Location = new Point(x, y); //sposta il puffo
+        }
+
+        private void SpostaAlberi()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int maxX = pnlArea.Width - pnlAlberi[i].Width;  // trova max in x
+                int maxY = pnlArea.Height - pnlAlberi[i].Height;  // trova max in y
+                int x = rand.Next(0, maxX + 1); // genera un numero casuale in x rispettando max
+                int y = rand.Next(0, maxY + 1); // genera un numero casuale in y rispettando max
+                posAlberi[i].x = x;  // AGGIORNA posAlberi
+                posAlberi[i].y = y;  // AGGIORNA posAlberi
+                pnlAlberi[i].Location = new Point(x, y); //sposta l'albero
+            }
+        }
+
+        private bool CollidePulse()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (pnlPuffo.Bounds.IntersectsWith(pnlAlberi[i].Bounds))
+                {
+                    // Ripristina la posizione precedente di Puffo
+                    pnlPuffo.Location = new Point(posPuffo.x - (pnlPuffo.Left - posPuffo.x), posPuffo.y - (pnlPuffo.Top - posPuffo.y));
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CollideGarga()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (pnlGarga.Bounds.IntersectsWith(pnlAlberi[i].Bounds))
+                {
+                    // Ripristina la posizione precedente di Garga
+                    pnlGarga.Location = new Point(posGarga.x - (pnlGarga.Left - posGarga.x), posGarga.y - (pnlGarga.Top - posGarga.y));
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
-
